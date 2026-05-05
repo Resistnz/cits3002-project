ffcb2d16ef85e287c1852ec0da30a76d25a7e94d
Resistnz <toby.fett@gmail.com>
2026-05-05 14:30:36 +0800

    good stuff

---
+++ main.py
@@ -0,0 +1,13 @@
+import config
+from protocols import DataLinkLayer, NetworkLayer, TransportLayer
+from devices import Host, Router
+
+def main():
+    hostA = Host()
+    hostB = Host()
+    router = Router()
+
+    
+
+if __name__ == "__main__":
+    main()
--- protocols.py
+++ protocols.py
@@ -1,8 +1,25 @@
 class DataLinkLayer():
-    ...
+    def __init__(self):
+        self.destination_mac : str = None
+        self.source_mac : str = None
+        self.type : int = None
+        self.payload : NetworkLayer = None
 
 class NetworkLayer():
-    ...
+    def __init__(self):
+        self.destination_ip : str = None
+        self.source_ip : str = None
+        self.ttl : int = None
+        self.protocol : int = None
+        self.total_length : int = None
+        self.payload : TransportLayer = None
 
 class TransportLayer():
-    ...
+    def __init__(self):
+        self.source_port : int = None
+        self.destination_port : int = None
+        self.length : int = None
+        self.checksum : int = None
+        self.type : int = None
+        self.sequence_number : int = None
+        self.data : str = None
